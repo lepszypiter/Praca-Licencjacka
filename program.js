bb99e0753f165f7b67346dadc4aaa013fdc5309d
lepszypiter <96298134+lepszypiter@users.noreply.github.com>
2024-03-29 14:25:12 +0100

    timer corrected

--- program.js
+++ program.js
@@ -6,3 +6,25 @@ function countClicks()
     count = count + item
     output.innerHTML = count
 }
+
+const startingMinutes = 1
+let time = startingMinutes * 60
+
+const countdownEl = document.getElementById('countdown')
+
+setInterval(updateCountdown, 1000)
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60)
+    let seconds = time % 60
+    seconds = seconds < 10 ? '0' + seconds : seconds
+
+    countdownEl.innerHTML = `${minutes}: ${seconds}`
+    time--
+    time = time < 0 ? 0 : time 
+}
+function redirectPage()
+{
+    window.location="file:///C:/Users/lepsz/OneDrive/Pulpit/Praca%20Licencjacka/Praca-Licencjacka/Wyniki.html"
+}
+setTimeout('redirectPage()', time * 1000 + 2000)

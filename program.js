ef79e6b8e1607b7b73972e169e7fb6af5b51ebdb
lepszypiter <96298134+lepszypiter@users.noreply.github.com>
2024-03-29 12:48:12 +0100

    save

--- program.js
+++ program.js
@@ -1,17 +1,8 @@
-var result = 0
-var buttonsArray = document.getElementsByClassName('Button')
-for (var i = 0; i < buttonsArray.length; i++) {
-    var button = buttonsArray[i]
-    button.addEventListener('click', function(event){
-        result++;
-    })
-}
-console.log(result)
-
 var count = 0
 var output = document.getElementById('output')
+var item = document.getElementsByName('grid-item-63')
 function countClicks()
 {
-    count = count + 1
+    count = count + item
     output.innerHTML = count
 }

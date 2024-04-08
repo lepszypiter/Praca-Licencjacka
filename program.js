77bc5376af34d79a003be626c12331bba5a91fc9
lepszypiter <96298134+lepszypiter@users.noreply.github.com>
2024-04-08 11:43:11 +0200

    Design

--- program.js
+++ program.js
@@ -3,7 +3,7 @@ var output = document.getElementById('output')
 var item = document.getElementsByName('grid-item-63')
 function countClicks()
 {
-    count = count + item
+    count = count + 1
     output.innerHTML = count
 }
 
@@ -12,19 +12,19 @@ let time = startingMinutes * 60
 
 const countdownEl = document.getElementById('countdown')
 
-setInterval(updateCountdown, 1000)
+// setInterval(updateCountdown, 1000)
 
-function updateCountdown() {
-    const minutes = Math.floor(time / 60)
-    let seconds = time % 60
-    seconds = seconds < 10 ? '0' + seconds : seconds
+// function updateCountdown() {
+//     const minutes = Math.floor(time / 60)
+//     let seconds = time % 60
+//     seconds = seconds < 10 ? '0' + seconds : seconds
 
-    countdownEl.innerHTML = `${minutes}: ${seconds}`
-    time--
-    time = time < 0 ? 0 : time 
-}
-function redirectPage()
-{
-    window.location="file:///C:/Users/lepsz/OneDrive/Pulpit/Praca%20Licencjacka/Praca-Licencjacka/Wyniki.html"
-}
-setTimeout('redirectPage()', time * 1000 + 2000)
+//     countdownEl.innerHTML = `${minutes}:${seconds}`
+//     time--
+//     time = time < 0 ? 0 : time 
+// }
+// function redirectPage()
+// {
+//     window.location="Wyniki.html"
+// }
+// setTimeout('redirectPage()', time * 1000 + 2000)

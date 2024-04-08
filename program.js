b2e3ce4fe782b4632cd08e9c1dd11d8a2cbe9fcc
lepszypiter <96298134+lepszypiter@users.noreply.github.com>
2024-04-08 13:44:35 +0200

    1. Działający test

--- program.js
+++ program.js
@@ -1,10 +1,17 @@
 var count = 0
 var output = document.getElementById('output')
-var item = document.getElementsByName('grid-item-63')
-function countClicks()
+
+var expectedHorizontal = 0
+var expectedVertical = 0
+
+
+function countClicks(elem)
 {
-    count = count + 1
+    console.log("countClicks elem" + elem);
+    checkTest(elem)
     output.innerHTML = count
+    console.log("countClicks count " + count);
+
 }
 
 const startingMinutes = 1
@@ -12,15 +19,31 @@ let time = startingMinutes * 60
 
 const countdownEl = document.getElementById('countdown')
 
+function checkTest(clickedId)
+{
+    console.log("checkTest expectedHorizontal " + expectedHorizontal);
+    console.log("checkTest expectedVertical " + expectedVertical);
+    console.log("checkTest clickedId " + clickedId);
+    var target = "Button-" + (expectedHorizontal + expectedVertical * 8)
+    console.log("checkTest target " + target);
+    if (clickedId == target)
+    {
+        console.log("checkTest OK ");
+        count = count + 1
+        nextTest()
+    }
+
+}
 function nextTest()
 {
-    var randHorizontal = getRandomIntInclusive(1, 7)
-    console.log("randHorizontal " + randHorizontal);
-    var itemHorizontal = document.getElementById('light-horizontal-' + randHorizontal)   
+    lightsOff()
+    expectedHorizontal = getRandomIntInclusive(1, 7)
+    console.log("randHorizontal " + expectedHorizontal);
+    var itemHorizontal = document.getElementById('light-horizontal-' + expectedHorizontal)   
     itemHorizontal.classList.add('light-on')
-    var randVertical = getRandomIntInclusive(1, 7)
-    console.log("randVertical " + randVertical);
-    var itemVertical = document.getElementById('light-vertical-' + randVertical)
+    expectedVertical = getRandomIntInclusive(1, 7)
+    console.log("randVertical " + expectedVertical);
+    var itemVertical = document.getElementById('light-vertical-' + expectedVertical)
     itemVertical.classList.add('light-on')
 }
 function getRandomIntInclusive(min, max) {
@@ -31,26 +54,28 @@ function getRandomIntInclusive(min, max) {
 function lightsOff()
 {
 
-    var lights = document.getElementsByClassName('light-on')
+    const lights = document.getElementsByClassName('light-on')
     console.log("lightsOff " + lights.length);
-    lights.forEach(element => {
-        element.classList.remove('light-on')
-    });
+    for (let i = 0; i < lights.length;) {
+        lights[i].classList.remove('light-on')
+      } 
 }
+
+nextTest()
   
-// setInterval(updateCountdown, 1000)
+setInterval(updateCountdown, 1000)
 
-// function updateCountdown() {
-//     const minutes = Math.floor(time / 60)
-//     let seconds = time % 60
-//     seconds = seconds < 10 ? '0' + seconds : seconds
+function updateCountdown() {
+    const minutes = Math.floor(time / 60)
+    let seconds = time % 60
+    seconds = seconds < 10 ? '0' + seconds : seconds
 
-//     countdownEl.innerHTML = `${minutes}:${seconds}`
-//     time--
-//     time = time < 0 ? 0 : time 
-// }
-// function redirectPage()
-// {
-//     window.location="Wyniki.html"
-// }
-// setTimeout('redirectPage()', time * 1000 + 2000)
+    countdownEl.innerHTML = `${minutes}:${seconds}`
+    time--
+    time = time < 0 ? 0 : time 
+}
+function redirectPage()
+{
+    window.location="Wyniki.html"
+}
+setTimeout('redirectPage()', time * 1000 + 2000)
